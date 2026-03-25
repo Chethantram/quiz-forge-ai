453d889cebeedf020f69bd3baefe0988bc79706c
Chethan T <chethant91@gmail.com>
2026-03-25 21:56:52 +0530

    update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,32 +3,37 @@ export default function sitemap() {
     {
       url: "https://quizforge.jo3.org",
       lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: "https://quizforge.jo3.org/about",
       lastModified: new Date(),
-      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://quizforge.jo3.org/generate-quiz",
       lastModified: new Date(),
-      changeFrequency: "weekly",
       priority: 0.9,
     },
     {
       url: "https://quizforge.jo3.org/sign-in",
       lastModified: new Date(),
-      changeFrequency: "yearly",
       priority: 0.5,
     },
     {
       url: "https://quizforge.jo3.org/sign-up",
       lastModified: new Date(),
-      changeFrequency: "yearly",
       priority: 0.5,
+    },
+    {
+      url: "https://quizforge.jo3.org/forgot-password",  
+      lastModified: new Date(),
+      priority: 0.4,
+    },
+    {
+      url: "https://quizforge.jo3.org/change-password",  
+      lastModified: new Date(),
+      priority: 0.3,
     },
   ]
 }
